5bf06612ce70746e35aaae9cf955958ae8796ce2
Davia Smickle <daviasmickle@gmail.com>
2022-10-10 00:35:23 -0500

    updated version

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -26,7 +26,7 @@ function plays(){
     const squaredivs = Array.from(document.querySelectorAll(".square"));
     const post = document.getElementById("status");
     var index;
-    for(const div of squaredivs); {
+    for(const div of squaredivs) {
         div.addEventListener("click", (e) =>{
             div.classList.add(currentTurn);
             div.textContent = currentTurn;
