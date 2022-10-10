8d89d7e2b1fc389d1daf7f42159c4e3310fc5c90
Davia Smickle <daviasmickle@gmail.com>
2022-10-09 22:39:36 -0500

    Add an X or O when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,13 +2,29 @@ document.addEventListener("DOMContentLoaded", loadDOM)
 
 function loadDOM(){ 
     showBoard() 
-    }
+}
 
-    function showBoard(){ 
-        const boarddivs = document.getElementById('board').getElementsByTagName('div');
-        
-        for ( i =0; i< boarddivs.length; i++){
-	        var divs = boarddivs[i];
-        	divs.classList.add("square");
-        }
-     }
+function showBoard(){ 
+    const boarddivs = document.getElementById('board').getElementsByTagName('div');
+
+    for ( i =0; i< boarddivs.length; i++){
+	    var divs = boarddivs[i];
+        divs.classList.add("square");
+    }
+}
+
+function plays(){
+
+    const squaredivs = Array.from(document.querySelectorAll(".square"));
+    const post = document.getElementById("status");
+    var index;
+    for(const div of squaredivs); {
+        div.addEventListener("click", (e) =>{
+            div.classList.add(currentTurn);
+            div.textContent = currentTurn;
+            index = squaredivs.indexOf(div);
+            checkArray.splice(index,1,currentTurn);
+            currentTurn = currentTurn == "X" ? "O" : "X";
+            }
+    }
+}
